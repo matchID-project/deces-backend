7e9547449864c1cd3ecb0cfbd815bf270efa3b6d
cristianpb <felipebrokate@gmail.com>
2020-02-21 09:22:15 +0100

    :alembic: Use axios library to request elasticsearch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import axios from 'axios';
 const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-  res.json({key:"value"});
-} );
+app.get( "/", async ( req, res ) => {
+  const result = await axios.get("http://elasticsearch:9200/deces")
+  console.log(result.data);
+  res.json(result.data);
+});
 
 // start the express server
 app.listen( port, () => {
