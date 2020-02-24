462887c7cef15fbf52da098d31fd045e252afb49
cristianpb <felipebrokate@gmail.com>
2020-02-24 09:37:15 +0100

    :building_construction: Use a post request to search in elasticsearch instead of a get

--- backend/src/runRequest.ts
+++ backend/src/runRequest.ts
@@ -2,10 +2,11 @@ import axios from 'axios';
 import Body from './types/body';
 
 export default async function runRequest(body: Body) {
-  const response = await axios.get("http://elasticsearch:9200/deces/_search", {
-    params: {
-      source: JSON.stringify(body),
-      source_content_type: 'application/json'
+  const response = await axios("http://elasticsearch:9200/deces/_search", {
+    method: 'post',
+    data: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
     }
   });
   if (response.status >= 400) {
