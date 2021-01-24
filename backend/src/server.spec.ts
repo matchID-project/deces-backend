48c8f0ab1d5ee81242a88559c532452dd1896350
cristianpb <felipebrokate@gmail.com>
2021-01-23 19:32:10 -0500

    🗑 Simplify server mocha test

--- backend/src/server.spec.ts
+++ backend/src/server.spec.ts
@@ -39,173 +39,92 @@ describe('server.ts - Express application', () => {
   });
 
   describe('/search GET', () => {
-    it('firstName', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({deathDate: 2020, firstName: 'Harry'})
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons[0].name.first).to.include('Harry');
-    });
+    const tests = [
+      {params: {deathDate: 2020, firstName: 'Harry'}, testFunc: (res: any) => {
+        expect(res.body.response.persons[0].name.first).to.include('Harry');
+      }},
+      {params: {deathDate: 2020, lastName: 'Pottier'}, testFunc: (res: any) => {
+        expect(res.body.response.persons[0].name.last).to.include('Pottier');
+      }},
+      {params: {deathDate: 2020, birthCountry: 'France'}, testFunc: (res: any) => {
+        expect(res.body.response.persons[0].birth.location.country).to.equal('France');
+      }},
+      {params: {deathDate: 2020, deathCountry: 'Argentine'}, testFunc: (res: any) => {
+        expect(res.body.response.persons[0].death.location.country).to.equal('Argentine');
+      }},
+      {params: {deathDate: 2020, birthDate: '23/01/1928'}, testFunc: (res: any) => {
+        expect(res.body.response.persons[0].birth.date).to.equal('19280123');
+      }},
+      {params: {deathDate: '22/01/2020'}, testFunc: (res: any) => {
+        expect(res.body.response.persons[0].death.date).to.equal('20200122');
+      }},
+      {params: {deathDate: '22/01/2020-30/01/2020'}, testFunc: (res: any) => {
+        res.body.response.persons.forEach((person: Person) => {
+          expect(parseInt(person.death.date, 10)).to.be.within(20200122, 20200130);
+        })
+      }},
+      {params: {deathDate: 2020, birthCity: 'Metz'}, testFunc: (res: any) => {
+        expect(res.body.response.persons[0].birth.location.city).to.equal('Metz');
+      }},
+      {params: {deathDate: 2020, deathCity: 'Nice'}, testFunc: (res: any) => {
+        expect(res.body.response.persons[0].death.location.city).to.equal('Nice');
+      }},
+      {params: {deathDate: 2020, birthDepartment: 57}, testFunc: (res: any) => {
+        expect(res.body.response.persons[0].birth.location.departmentCode).to.equal('57');
+      }},
+      {params: {deathDate: 2020, deathDepartment: 75}, testFunc: (res: any) => {
+        expect(res.body.response.persons[0].death.location.departmentCode).to.equal('75');
+      }},
+      {params: {deathDate: 2020, firstName: 'Ana', fuzzy: false}, testFunc: (res: any) => {
+        res.body.response.persons.forEach((person: Person) => {
+          expect(person.name.first).to.include('Ana');
+        })
+      }},
+      {params: {q: 'Michel Rojo'}, testFunc: (res: any) => {
+        res.body.response.persons.forEach((person: Person) => {
+          expect(person.name.first).to.include('Michel');
+        })
+      }},
+      {params: {}, testFunc: (res: any) => {
+        expect(res.body.msg).to.include('error');
+      }, status: 400},
+      {params: { bob: 'Pop' }, testFunc: (res: any) => {
+        expect(res.body.msg).to.include('error');
+      }, status: 400},
+      {params: { birthDate: 19 }, testFunc: (res: any) => {
+        res.body.msg.some((msg: string) => {
+          expect(msg).to.include('invalid');
+        })
+      }, status: 400},
+      {params: { birthDate: 19, q: 'Georges' }, testFunc: (res: any) => {
+        res.body.msg.some((msg: string) => {
+          expect(msg).to.include('invalid');
+        })
+      }, status: 400},
+      {params: { deathDate: '2020', deathAge: 20 }, testFunc: (res: any) => {
+        expect(res.body.response.persons).to.have.lengthOf.within(1, 20);
+      }},
+      {params: { deathDate: '2020', sex: 'M' }, testFunc: (res: any) => {
+        expect(res.body.response.persons.map((x: Person) => x.sex)).to.not.include('F');
+      }},
+      {params: { deathDate: '2020', sort: '[{\"sex\":\"asc\"}]' }, testFunc: (res: any) => {
+        expect(res.body.response.persons.map((x: Person) => x.sex)).to.not.include('M');
+      }},
+      {params: { firstName: 'Inconnu' }, testFunc: (res: any) => {
+        expect(res.body.response.persons).to.have.lengthOf(0);
+      }}
+    ];
 
-    it('lastName', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({deathDate: 2020, lastName: 'Pottier'})
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons[0].name.last).to.include('Pottier');
-    });
-
-    it('birthCountry', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({deathDate: 2020, birthCountry: 'France'})
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons[0].birth.location.country).to.equal('France');
-    });
-
-    it('deathCountry', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({deathDate: 2020, deathCountry: 'Argentine'})
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons[0].death.location.country).to.equal('Argentine');
-    });
-
-    it('birthDate', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({deathDate: 2020, birthDate: '23/01/1928'})
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons[0].birth.date).to.equal('19280123');
-    });
-
-    it('deathDate', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ deathDate: '22/01/2020'})
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons[0].death.date).to.equal('20200122');
-    });
-
-    it('deathDate range', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ deathDate: '22/01/2020-30/01/2020'})
-      expect(res).to.have.status(200);
-      res.body.response.persons.forEach((person: Person) => {
-        expect(parseInt(person.death.date, 10)).to.be.within(20200122, 20200130);
-      })
-    });
-
-    it('birthCity', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ deathDate: '2020', birthCity: 'Metz' })
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons[0].birth.location.city).to.equal('Metz');
-    });
-
-    it('deathCity', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ deathDate: '2020', deathCity: 'Nice' })
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons[0].death.location.city).to.equal('Nice');
-    });
-
-    it('birthDepartment Code', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ deathDate: '2020', birthDepartment: 57 })
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons[0].birth.location.departmentCode).to.equal('57');
-    });
-
-    it('deathDepartment Code', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ deathDate: '2020', deathDepartment: 75 })
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons[0].death.location.departmentCode).to.equal('75');
-    });
-
-    it('fuzzy', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ deathDate: '2020', firstName: 'Ana', fuzzy: false })
-      expect(res).to.have.status(200);
-      res.body.response.persons.forEach((person: Person) => {
-        expect(person.name.first).to.include('Ana');
-      })
-    });
-
-    it('fullText', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ q: 'Michel Rojo' })
-      expect(res).to.have.status(200);
-      res.body.response.persons.forEach((person: Person) => {
-        expect(person.name.first).to.include('Michel');
-      })
-    });
-
-    it('empty request', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-      expect(res).to.have.status(400);
-      expect(res.body.msg).to.include('error');
-    });
-
-    it('wrong field', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ bob: 'Pop' })
-      expect(res).to.have.status(400);
-      expect(res.body.msg).to.include('error');
-    });
-
-    it('wrong value', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ birthDate: 19 })
-      expect(res).to.have.status(400);
-      res.body.msg.some((msg: string) => {
-        expect(msg).to.include('invalid');
-      })
-    });
-
-    it('simple and complex request', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ birthDate: 19, q: 'Georges' })
-      expect(res).to.have.status(400);
-      res.body.msg.some((msg: string) => {
-        expect(msg).to.include('invalid');
-      })
-    });
-
-    it('deathAge', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ deathDate: '2020', deathAge: 20 })
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons).to.have.lengthOf.within(1, 20);
-    });
-
-    it('sex', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ deathDate: '2020', sex: 'M' })
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons.map((x: Person) => x.sex)).to.not.include('F');
-    });
-
-    it('sort', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ deathDate: '2020', sort: '[{\"sex\":\"asc\"}]' })
-      expect(res).to.have.status(200);
-      expect(res.body.response.persons.map((x: Person) => x.sex)).to.not.include('M');
+    tests.forEach((test) => {
+      it(`${Object.entries(test.params).join(" ")}`, async () => {
+        const res = await chai.request(app)
+          .get(`${process.env.BACKEND_PROXY_PATH}/search`)
+          .query(test.params)
+        expect(res).to.have.status(test.status ? test.status : 200);
+        if (test.testFunc) {
+          test.testFunc(res)
+        }
+      });
     });
 
     it('scroll', async () => {
@@ -240,15 +159,6 @@ describe('server.ts - Express application', () => {
         totalPersons += res.body.response.persons.length;
       }
       expect(totalPersons).to.equal(res.body.response.total);
-    });
-
-    it('inconnu', async () => {
-      const res = await chai.request(app)
-        .get(apiPath('search'))
-        .query({ firstName: 'Inconnu' })
-      expect(res).to.have.status(200);
-      expect(res.body.response.total).to.equal(0);
-      expect(res.body.response.persons).to.have.lengthOf(0);
     });
 
   })
