84973fb08f09f73bdd3739a5d45e3acb36bfe975
cristianpb <felipebrokate@gmail.com>
2021-02-16 04:14:41 -0500

    🐛 Fix swagger parsing for query parameters

--- backend/src/controllers/job.controller.ts
+++ backend/src/controllers/job.controller.ts
@@ -6,31 +6,35 @@ export class JobsController extends Controller {
 
 
   @Tags('Jobs')
-  @Get('/:queueName(jobs|chunks)/jobs')
+  @Get('/{queueName}')
   public async jobName(
     @Path() queueName: 'jobs'|'chunks' = 'jobs'
   ): Promise<any> {
-    const jobQueue = new Queue(queueName,  {
-      redis: {
-        host: 'redis'
-      }
-    });
-    const jobs = await jobQueue.checkHealth();
-    const checkStalledJobs = new Promise((resolve, reject) => {
-      jobQueue.checkStalledJobs((err, numStalled) => { 
-        if (err) reject(err.toString())
-        resolve(numStalled) 
-      })
-    });
-    const stalled = await checkStalledJobs
-    return { ...jobs, stalled };
+    if (queueName !== undefined) {
+      const jobQueue = new Queue(queueName,  {
+        redis: {
+          host: 'redis'
+        }
+      });
+      const jobs = await jobQueue.checkHealth();
+      const checkStalledJobs = new Promise((resolve, reject) => {
+        jobQueue.checkStalledJobs((err, numStalled) => {
+          if (err) reject(err.toString())
+          resolve(numStalled)
+        })
+      });
+      const stalled = await checkStalledJobs
+      return { ...jobs, stalled };
+    } else {
+      return {msg: 'available queues: "chunks" and "jobs"'};
+    }
   }
 
   @Tags('Jobs')
-  @Get('/:queueName(jobs|chunks)/jobs/:jobsType(waiting|active|delayed|succeeded|failed)')
+  @Get('/{queueName}/{jobsType}')
   public async getJobs(
     @Path() queueName: 'jobs'|'chunks',
-    @Path() jobsType: 'waiting'|'active'|'delayed'|'succeeded'|'failed',
+    @Path() jobsType: string,
     @Query() size?: number,
     @Query() start?: number,
     @Query() end?: number,
@@ -40,13 +44,23 @@ export class JobsController extends Controller {
         host: 'redis'
       }
     });
-    const page = { 
-      size: size || 20,
-      start: start || 0,
-      end: end || 25
+    if (['waiting', 'active', 'delayed', 'succeeded', 'failed'].includes(jobsType)) {
+      const page = {
+        size: size || 20,
+        start: start || 0,
+        end: end || 25
+      }
+      const jobs = await jobQueue.getJobs(jobsType, page)
+      return { jobs };
+    } else {
+      const jobs = await jobQueue.getJob(jobsType)
+      if (jobs) {
+        return { jobs };
+      } else {
+        return { msg: 'job not found' };
+      }
     }
-    const jobs = await jobQueue.getJobs(jobsType, page)
-    return { jobs };
+
   }
 
 }
