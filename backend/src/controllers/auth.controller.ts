de8a07f44dad571f96057afcfbd8b5436fe213b8
cristianpb <felipebrokate@gmail.com>
2021-03-20 00:45:16 +0100

    🔒 Use json array for user authentification

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import * as jwt from "jsonwebtoken";
 import {Body, Controller, Post, Route, Tags} from 'tsoa';
+import {userDB} from '../userDB';
 
 /**
  * @swagger
@@ -21,6 +22,9 @@ export class AuthController extends Controller {
   ): AccessToken {
     if (jsonToken.password === process.env.BACKEND_TOKEN_PASSWORD) {
       const accessToken = jwt.sign({...jsonToken, scopes: ['admin']}, process.env.BACKEND_TOKEN_KEY, { expiresIn: "1d" })
+      return { 'access_token': accessToken }
+    } else if (userDB.includes(jsonToken.password)) {
+      const accessToken = jwt.sign({...jsonToken, scopes: ['user']}, process.env.BACKEND_TOKEN_KEY, { expiresIn: "1d" })
       return { 'access_token': accessToken }
     } else {
       this.setStatus(400);
