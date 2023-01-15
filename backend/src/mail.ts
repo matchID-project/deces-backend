282f2ef725c3673c2130ac8e9ac2998bb35b0e5b
cristianpb <felipebrokate@gmail.com>
2023-01-15 17:51:58 +0100

    📦 Use nodemailer instead of emailjs Update dependencies in package.json

--- backend/src/mail.ts
+++ backend/src/mail.ts
@@ -1,10 +1,11 @@
-import { SMTPClient } from 'emailjs';
+import nodemailer  from 'nodemailer';
 import { ReviewStatus } from './models/entities';
 
-const client = new SMTPClient({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT)
-});
+const mailConfig = {
+     host: process.env.SMTP_HOST,
+     port: Number(process.env.SMTP_PORT),
+ };
+const transporter = nodemailer.createTransport(mailConfig);
 
 const OTP:any = {};
 
@@ -13,7 +14,7 @@ const generateOTP = (email: string) => {
     let tmp = '';
     for (let i = 0; i < 6; i++ ) {
         tmp += digits[Math.floor(Math.random() * 10)];
-    };
+    }
     OTP[email] = tmp;
     setTimeout(() => {delete OTP[email]}, 600000);
 }
@@ -21,7 +22,7 @@ const generateOTP = (email: string) => {
 export const sendOTP = async (email: string): Promise<boolean> => {
     try {
         generateOTP(email);
-        await client.sendAsync({
+        await transporter.sendMail({
             subject: `Validez votre identité - ${process.env.APP_DNS}`,
             text: `Votre code, valide 10 minutes: ${OTP[email] as string}`,
             from: process.env.API_EMAIL,
@@ -72,7 +73,7 @@ export const sendUpdateConfirmation = async (email:string, status: ReviewStatus,
             </html>
             `, alternative: true};
         }
-        await client.sendAsync(message);
+        await transporter.sendMail(message);
         return true;
     } catch (err) {
         return false;
